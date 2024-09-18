816f1f98e1cc2bbefc3f11a88f15119990722d1a
Tamara213020 <tamara.onchevska@students.finki.ukim.mk>
2024-09-18 18:33:09 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+#C:\Users\Tamara\Desktop\kol2022-g1-Employee\kol2022-g1-Employee\target\g1-0.0.1-SNAPSHOT.jar
+FROM openjdk:17-jdk-alpine
+WORKDIR /app
+COPY target/g1-0.0.1-SNAPSHOT.jar app.jar
+EXPOSE 8080
+ENTRYPOINT ["java", "-jar", "app.jar"]
---
+++ src/main/java/mk/ukim/finki/wp/kol2022/g1/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package mk.ukim.finki.wp.kol2022.g1.repository;
+
+import mk.ukim.finki.wp.kol2022.g1.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository  extends JpaRepository<Employee, Long> {
+}
